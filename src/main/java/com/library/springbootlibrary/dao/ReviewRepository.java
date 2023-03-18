74ae0cb125e37b443357feb4a3d255e2651a5e86
Jiaqi Zhao <jiaqi.zhao24@gmail.com>
2023-03-17 23:02:10 -0700

    add reviews on checkout page

---
+++ src/main/java/com/library/springbootlibrary/dao/ReviewRepository.java
@@ -0,0 +1,13 @@
+package com.library.springbootlibrary.dao;
+
+import com.library.springbootlibrary.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByBookId(@RequestParam("bookId") Long bookId,
+                              Pageable pageable);
+}
