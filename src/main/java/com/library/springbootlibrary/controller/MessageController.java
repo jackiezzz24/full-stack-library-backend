c4cbc3319ab663e95fee2002693cf233c06fec45
Jiaqi Zhao <jiaqi.zhao24@gmail.com>
2023-05-08 14:08:27 +0800

    feat: add messages features

---
+++ src/main/java/com/library/springbootlibrary/controller/MessageController.java
@@ -0,0 +1,28 @@
+package com.library.springbootlibrary.controller;
+
+
+import com.library.springbootlibrary.entity.Message;
+import com.library.springbootlibrary.service.MessageService;
+import com.library.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    private MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/secure/add/message")
+    public void postMessage(@RequestHeader(value="Authorization") String token,
+                            @RequestBody Message messageRequest) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messageService.postMessage(messageRequest, userEmail);
+    }
+}
